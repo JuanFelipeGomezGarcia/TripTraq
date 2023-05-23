657b4615e2fbc675c87f9e0cb6b4227a71dcbb5b
sebas <smora1110@gmail.com>
2023-05-23 17:45:23 -0500

    lo que gomita hace (nada)

--- app/src/main/java/com/example/triptraq/MainActivity.java
+++ app/src/main/java/com/example/triptraq/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.triptraq;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -16,6 +17,7 @@ import com.android.volley.NetworkError;
 import com.android.volley.NoConnectionError;
 import com.android.volley.ParseError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.ServerError;
 import com.android.volley.TimeoutError;
@@ -37,53 +39,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         pt_usuario = findViewById(R.id.pt_usuario);
         pt_contraseña = findViewById(R.id.pt_contraseña);
-        Btn_Iniciar =findViewById(R.id.bt_login);
+        Btn_Iniciar=findViewById(R.id.bt_login);
 
 
     }
-    public void login(View v) {
-        String usuario = pt_usuario.getText().toString().trim();
-        String contrasena = pt_contraseña.getText().toString().trim();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://triptrack.eastus.cloudapp.azure.com/getlogin.php" + "?usuario=" + usuario + "&contrasena=" + contrasena,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        if (response.equals("Login exitoso")) {
-                            // Login exitoso, pasar a otra actividad
-                            Toast.makeText(MainActivity.this, "Login exitoso", Toast.LENGTH_SHORT).show();
-                            // Aquí puedes iniciar la otra actividad
-                        } else {
-                            // Usuario o contraseña incorrectos
-                            Toast.makeText(MainActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        String errorMessage = "Error de conexión";
-
-                        if (error instanceof NetworkError) {
-                            errorMessage = "Error de red";
-                        } else if (error instanceof ServerError) {
-                            errorMessage = "Error del servidor";
-                        } else if (error instanceof AuthFailureError) {
-                            errorMessage = "Error de autenticación";
-                        } else if (error instanceof ParseError) {
-                            errorMessage = "Error de análisis";
-                        } else if (error instanceof NoConnectionError) {
-                            errorMessage = "Sin conexión";
-                        } else if (error instanceof TimeoutError) {
-                            errorMessage = "Tiempo de espera agotado";
-                        }
-
-                        // Mostrar el mensaje de error en el Toast
-                        Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        // Agregar la solicitud a la cola de solicitudes
-        Volley.newRequestQueue(this).add(stringRequest);
+    public void login(View view){
+        Toast.makeText(MainActivity.this,"Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+        validarUsuario("http://triptrack.eastus.cloudapp.azure.com/login.php");
+    }
+    private void validarUsuario(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()){
+                    Intent intent=new Intent(getApplicationContext(), Menu.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,"Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(),Toast.LENGTH_SHORT).show();}
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String,String>();
+                parametros.put("usuario",pt_usuario.getText().toString());
+                parametros.put("contrasena",pt_contraseña.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue=Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
