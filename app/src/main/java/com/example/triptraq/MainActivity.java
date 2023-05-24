094652907c5e1d95571d069716c057f9fcfafa0e
sebas <smora1110@gmail.com>
2023-05-23 20:49:49 -0500

    Login hecho

--- app/src/main/java/com/example/triptraq/MainActivity.java
+++ app/src/main/java/com/example/triptraq/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText pt_usuario;
     private EditText pt_contraseña;
     private CardView Btn_Iniciar;
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,38 +49,46 @@ public class MainActivity extends AppCompatActivity {
         pt_contraseña = findViewById(R.id.pt_contraseña);
         Btn_Iniciar=findViewById(R.id.bt_login);
 
+    }
+        public void onClick(View v) {
+            String username = pt_usuario.getText().toString();
+            String password = pt_contraseña.getText().toString();
+
+            String url = "http://triptrack.eastus.cloudapp.azure.com/login.php?username=" + URLEncoder.encode(username) + "&password=" + URLEncoder.encode(password);
+            new LoginTask().execute(url);
 
     }
-    public void login(View view){
-        Toast.makeText(MainActivity.this,"Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
-        validarUsuario("http://triptrack.eastus.cloudapp.azure.com/login.php");
-    }
-    private void validarUsuario(String URL){
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if(!response.isEmpty()){
-                    Intent intent=new Intent(getApplicationContext(), Menu.class);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(MainActivity.this,"Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result += line;
                 }
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.toString(),Toast.LENGTH_SHORT).show();}
-        }){
-            @Nullable
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> parametros=new HashMap<String,String>();
-                parametros.put("usuario",pt_usuario.getText().toString());
-                parametros.put("contrasena",pt_contraseña.getText().toString());
-                return parametros;
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result.equals("success")) {
+                // Inicio de sesión exitoso
+                Intent intent =new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                // Continuar con la lógica de tu aplicación
+            } else {
+                // Inicio de sesión fallido
+                Toast.makeText(MainActivity.this, "Inicio de sesión fallido", Toast.LENGTH_SHORT).show();
             }
-        };
-        RequestQueue requestQueue=Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        }
     }
 }
